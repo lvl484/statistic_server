2cb57879f001f3d7f13b45ea969f9dcffd2f90a6
Kateryna <okkerri@gmail.com>
2020-02-18 19:15:34 +0200

    add simulating

--- database.go
+++ database.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"os"
 	"log"
 	"net/http"
-	"database/sql"
-	"encoding/json"
-    "errors"
-    "io"
-    "strings"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //DBmetric - stuct for working with metrics in database
@@ -18,7 +16,9 @@ type DBmetric struct {
 }
 
 func newDBmetric() *DBmetric {
+
 	s := fmt.Sprintf("%v:%v@/%v", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASS"), os.Getenv("MYSQL_DB"))
+
 	database, err := sql.Open("mysql", s)
 
 	if err != nil {
@@ -32,70 +32,46 @@ func newDBmetric() *DBmetric {
 
 //MetricsCreate record metris from endpoints end record it to db
 func (database *DBmetric) MetricsCreate(w http.ResponseWriter, r *http.Request) {
-	var metrics Metrics
-	if r.Header.Get("Content-Type") != "" {
-        value, _ := ParseValueAndParams(r.Header, "Content-Type")
-        if value != "application/json" {
-            msg := "Content-Type header is not application/json"
-            http.Error(w, msg, http.StatusUnsupportedMediaType)
-            return
-        }
-    }
+	//var metrics Metrics
 
-    r.Body = http.MaxBytesReader(w, r.Body, 1048576)
-
-    dec := json.NewDecoder(r.Body)
-    dec.DisallowUnknownFields()
-
-    err := dec.Decode(&metrics)
-    if err != nil {
-        var syntaxError *json.SyntaxError
-        var unmarshalTypeError *json.UnmarshalTypeError
-
-        switch {
-        case errors.As(err, &syntaxError):
-            msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-            http.Error(w, msg, http.StatusBadRequest)
-
-        case errors.Is(err, io.ErrUnexpectedEOF):
-            msg := fmt.Sprintf("Request body contains badly-formed JSON")
-            http.Error(w, msg, http.StatusBadRequest)
-
-        case errors.As(err, &unmarshalTypeError):
-            msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
-            http.Error(w, msg, http.StatusBadRequest)
-
-        case strings.HasPrefix(err.Error(), "json: unknown field "):
-            fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-            msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
-            http.Error(w, msg, http.StatusBadRequest)
-
-        case errors.Is(err, io.EOF):
-            msg := "Request body must not be empty"
-            http.Error(w, msg, http.StatusBadRequest)
-
-        case err.Error() == "http: request body too large":
-            msg := "Request body must not be larger than 1MB"
-            http.Error(w, msg, http.StatusRequestEntityTooLarge)
-
-        default:
-            log.Println(err.Error())
-            http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-        }
-        return
-    }
-
-    if dec.More() {
-        msg := "Request body must only contain a single JSON object"
-        http.Error(w, msg, http.StatusBadRequest)
-        return
-    }
-
-	_, err = database.db.Exec(
-		"INSERT INTO metrics(ServiceName,MetricName,MetricValue) VALUES(?,?,?)",
-		metrics.ServiceName, metrics.MetricName, metrics.MetricValue)
-	if err != nil {
-		log.Println(err)
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "InvalidMethod")
 		return
 	}
+
+	r.ParseForm()
+
+	//
+	log.Println(r.Form)
+
+	for key, values := range r.Form {
+		for _, value := range values {
+			fmt.Println(key, value)
+		}
+	}
+	//
+
+	/* fmt.Fprintf(w, r.Form.Get("name"))
+	}
+
+		err := json.NewDecoder(r.Body).Decode(&metrics)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+	// Exec executes a query without returning any rows.
+	// The args are for any placeholder parameters  in the query.
+		_, err = database.db.Exec(
+			"INSERT INTO users(ServiceName,MetricName,MetricValue) VALUES(?,?,?,?)",
+	        metrics.ServiceName, metrics.MetricName, metrics.MetricValue)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	*/
 }
--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
-	"flag"
 )
 
 func main() {
 	address := flag.String("address", ":80", "address of server")
 	flag.Parse()
 
-	dbHandling := newDBmetric()	
+	dbHandling := newDBmetric()
 	defer dbHandling.db.Close()
 
 	router := mux.NewRouter()
 	router.Use(Middleware)
 
 	api := router.PathPrefix("/api/v1").Subrouter()
-	api.HandleFunc("/{metric}", dbHandling.MetricsCreate).Methods(http.MethodGet)
-	
+	//	api.HandleFunc("/{metric}", dbHandling.MetricsCreate).Methods(http.MethodGet)
+	api.HandleFunc("/", dbHandling.MetricsCreate).Methods(http.MethodGet)
+
 	if err := http.ListenAndServe(*address, router); err != nil {
 		log.Fatal(err.Error())
 	}
-	 
+
 }
